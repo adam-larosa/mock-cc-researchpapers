58fc6413b57b5faa843330b2022492d96d2d7f0b
Adam La Rosa <adam.larosa@flatironschool.com>
2023-04-12 13:19:39 -0700

    lil s solution

--- server/app.py
+++ server/app.py
@@ -2,27 +2,87 @@
 
 from flask import Flask, make_response, jsonify, request
 from flask_migrate import Migrate
-from flask_restful import Api
+from flask_restful import Api, Resource
 
-from models import db, Research, Author, ResearchAuthors
+from models import db, Research, Author, ResearchAuthor
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.json.compact = False
-
 migrate = Migrate(app, db)
-
 db.init_app(app)
+
+api = Api( app )
+
+class Researches( Resource ):
+
+    def get( self ):
+        r_list = []
+        for r in Research.query.all():
+            r_dict = {
+                'id': r.id,
+                'topic': r.topic,
+                'year': r.year,
+                'page count': r.page_count
+            }
+            r_list.append( r_dict )
+        return make_response( r_list, 200 )
+
+api.add_resource( Researches, '/research' )
+
+
+class ResearchesById( Resource ):
+    def get( self, id ):
+        r_instance = Research.query.filter_by( id = id ).first()
+        if r_instance == None:
+            return make_response( { 'error': 'research not found' }, 404 )
+        return make_response( r_instance.to_dict(), 200 )
+
+    def delete( self, id ):
+        r_instance = Research.query.filter_by( id = id ).first()
+        if r_instance == None:
+            return make_response( { 'error': 'research not found' }, 404 )
+
+        # The "for" loop which would replace cascade = 'all, delete-orphan'
+        # in the Model.
+
+        # for ra in r_instance.research_authors:
+        #     db.session.delete( ra )
+        #     db.session.commit()
+        
+        db.session.delete( r_instance )
+        db.session.commit()
+        return make_response( "MEOWZER!", 204 )
+
+api.add_resource( ResearchesById, '/research/<int:id>')
+
+class Authors( Resource ):
+    def get( self ):
+        a_list = [ a.to_dict() for a in Author.query.all() ]
+        return make_response( a_list, 200 ) 
+
+api.add_resource( Authors, '/authors' )
+
+
+class ResearchAuthors( Resource ):
+    def post( self ):
+        data = request.get_json()
+        ra = ResearchAuthor( author_id = data['author_id'], 
+                             research_id = data['research_id'] )
+        db.session.add( ra )
+        db.session.commit()
+        return make_response( ra.author.to_dict(), 201 )
+
+api.add_resource( ResearchAuthors, '/research_author' )
+
+
 
 @app.route('/')
 def index():
     return '<h1>Code challenge</h1>'
 
-@app.route('/research')
-def restaurants():
 
-    pass
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
--- server/models.py
+++ server/models.py
@@ -8,6 +8,56 @@ metadata = MetaData(naming_convention={
     "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
 })
 
-db = SQLAlchemy(metadata=metadata)
+db = SQLAlchemy( metadata = metadata)
 
-# Add models here
+#           research -----< research_author >----- author
+
+
+class ResearchAuthor( db.Model, SerializerMixin ):
+    __tablename__ = 'research_authors' 
+    id = db.Column( db.Integer, primary_key = True )
+    author_id = db.Column( db.Integer, db.ForeignKey( 'authors.id' ) )
+    research_id = db.Column( db.Integer, db.ForeignKey( 'researches.id' ) )
+
+
+class Research( db.Model, SerializerMixin ):
+    __tablename__ = 'researches'
+
+    serialize_rules = ( '-research_authors', 'authors' )
+
+    id = db.Column( db.Integer, primary_key = True )
+    topic = db.Column( db.String )
+    year = db.Column( db.Integer )
+    page_count = db.Column( db.Integer )
+
+    research_authors = db.relationship( 'ResearchAuthor', backref = 'research',
+                                        cascade = 'all, delete-orphan' )
+    authors = association_proxy( 'research_authors', 'author' )
+
+    @validates( 'year' )
+    def validate_year( self, key, year_integer ):
+        if len( str( year_integer ) ) != 4:
+            raise ValueError( 'year must be four digits' )
+        return year_integer
+
+
+
+class Author( db.Model, SerializerMixin ):
+    __tablename__ = 'authors'
+
+    serialize_rules = ( '-research_authors', )
+
+    id = db.Column( db.Integer, primary_key = True )
+    name = db.Column( db.String )
+    field_of_study = db.Column( db.String )
+
+    research_authors = db.relationship( 'ResearchAuthor', backref = 'author' )
+    researches = association_proxy( 'research_authors', 'research' )
+
+    @validates( 'field_of_study' )
+    def validate_study( self, key, field_string ):
+        fields = [ 'AI', 'Robotics', 'Machine Learning', 'Vision', 
+                   'Cybersecurity' ]
+        if field_string not in fields:
+            raise ValueError( 'study something different!!!' )
+        return field_string
